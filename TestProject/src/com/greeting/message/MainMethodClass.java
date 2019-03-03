e8cfd1bf6a17fc0f8d559005d396727352694ff4
Ajitesh Maity <ajiteshmaity@gmail.com>
2019-03-03 21:44:16 +0530

    Greetings Added private method for greeting

--- TestProject/src/com/greeting/message/MainMethodClass.java
+++ TestProject/src/com/greeting/message/MainMethodClass.java
@@ -3,7 +3,12 @@ package com.greeting.message;
 public class MainMethodClass {
 
 	public static void main(String[] args) {
-		System.out.println("Hello world...");
+		new MainMethodClass().greet();
+
+	}
+
+	private void greet() {
+		System.out.println("Hello world from method");
 
 	}
 
